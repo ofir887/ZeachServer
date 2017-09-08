66a66cab33409d02d65839160a7af6b568b700e0
ofir887 <ofir887@users.noreply.github.com>
2017-09-08 18:49:05 +0300

    Tabs design

--- test.py
+++ test.py
@@ -3,10 +3,7 @@ from fbprophet import Prophet
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
-import _thread as thread
-import threading
 import datetime
-import json
 from apscheduler.schedulers.blocking import BlockingScheduler
 
 
@@ -14,19 +11,18 @@ class BeachListener(object):
     Country = ""
     CurrentCount = 0
     BeachID = ""
-    BeachListenerID=""
+    BeachListenerID = ""
 
-    def __init__(self, country, count,beachListenerId):
+    def __init__(self, country, count, beachListenerId):
         self.Country = country
         self.CurrentCount = count
         self.BeachListenerID = beachListenerId
 
-    def setBeachID(self,id):
+    def setBeachID(self, id):
         self.BeachID = id
 
     def print(self):
         print(self.BeachID + " " + self.Country + " " + str(self.CurrentCount) + " " + self.BeachListenerID)
-
 
 
 def stream_handler(message):
@@ -35,11 +31,12 @@ def stream_handler(message):
     paths = paths.split('/')
     if (paths.__len__() > 2):
         newCount = int(message['data'])
-      #  beachId = data.child("BeachesListener/Country/" + paths[1] + "/" + paths[2] + "/BeachID").get().val()
-        beach = BeachListener(paths[1], newCount,paths[2])
-        beachId = data.child("BeachesListener/Country/" + beach.Country + "/" + beach.BeachListenerID + "/BeachID").get().val()
+        #  beachId = data.child("BeachesListener/Country/" + paths[1] + "/" + paths[2] + "/BeachID").get().val()
+        beach = BeachListener(paths[1], newCount, paths[2])
+        beachId = data.child(
+            "BeachesListener/Country/" + beach.Country + "/" + beach.BeachListenerID + "/BeachID").get().val()
         beach.setBeachID(beachId)
-        #beachId = data.child("BeachesListener/Country/" + beach.Country + "/" + beach.BeachID + "/BeachID").get().val()
+        # beachId = data.child("BeachesListener/Country/" + beach.Country + "/" + beach.BeachID + "/BeachID").get().val()
         beach.print()
         newBeachCountValueByGps = beach.CurrentCount;
         CurrentHour = datetime.datetime.now().hour;
@@ -53,7 +50,7 @@ def stream_handler(message):
         else:
             if (int(newBeachCountValueByGps) < int(predictedHourValue.val())):
                 newValue = (int(newBeachCountValueByGps)) + int(predictedHourValue.val());
-                newValue = newValue/2*1.5
+                newValue = newValue / 2 * 1.5
         data.child("Beaches/Country/" + beach.Country + "/" + beach.BeachID + "/Result").set(newValue)
         data.child("Beaches/Country/" + beach.Country + "/" + beach.BeachID + "/Current People").set(
             int(newBeachCountValueByGps))
