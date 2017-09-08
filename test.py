9cc01ec0ed1f21fb7906d7089b094da45b92820d
ofir887 <ofir887@users.noreply.github.com>
2017-09-08 18:43:25 +0300

    fix stream listener and fit to new data structure

--- test.py
+++ test.py
@@ -6,57 +6,83 @@ import matplotlib.pyplot as plt
 import _thread as thread
 import threading
 import datetime
+import json
 from apscheduler.schedulers.blocking import BlockingScheduler
+
+
+class BeachListener(object):
+    Country = ""
+    CurrentCount = 0
+    BeachID = ""
+    BeachListenerID=""
+
+    def __init__(self, country, count,beachListenerId):
+        self.Country = country
+        self.CurrentCount = count
+        self.BeachListenerID = beachListenerId
+
+    def setBeachID(self,id):
+        self.BeachID = id
+
+    def print(self):
+        print(self.BeachID + " " + self.Country + " " + str(self.CurrentCount) + " " + self.BeachListenerID)
 
 
 
 def stream_handler(message):
-    print(message) # {'title': 'Pyrebase', "body": "etc..."}
-    print(message["path"])
-    str = message["path"]
-    newBeachCountValueByGps = message["data"];
-    CurrentHour = datetime.datetime.now().hour;
-    strMixed = "Beaches/Country/Israel/Tzok/Hours/"+CurrentHour.__str__()+"/"
-    predictedHourValue = data.child(strMixed).get()
-    print(predictedHourValue.val())
-    if (str !='111'):
-        newValue=0;
+    print(message)  # {'title': 'Pyrebase', "body": "etc..."}
+    paths = str(message['path'])
+    paths = paths.split('/')
+    if (paths.__len__() > 2):
+        newCount = int(message['data'])
+        beachId = data.child("BeachesListener/Country/" + paths[1] + "/" + paths[2] + "/BeachID").get().val()
+        beach = BeachListener(paths[1], newCount,paths[2])
+        beach.setBeachID(beachId)
+        beachId = data.child("BeachesListener/Country/" + beach.Country + "/" + beach.BeachID + "/BeachID").get().val()
+        beach.print()
+        newBeachCountValueByGps = beach.CurrentCount;
+        CurrentHour = datetime.datetime.now().hour;
+        strMixed = "Beaches/Country/" + beach.Country + "/" + beach.BeachID + "/Hours/" + CurrentHour.__str__() + "/"
+        predictedHourValue = data.child(strMixed).get()
+        print(strMixed)
+        print(predictedHourValue.val())
+        newValue = 0;
         if (int(newBeachCountValueByGps) > int(predictedHourValue.val())):
-            newValue = (int(newBeachCountValueByGps))*1.05;
+            newValue = (int(newBeachCountValueByGps)) * 1.05;
         else:
             if (int(newBeachCountValueByGps) < int(predictedHourValue.val())):
-                newValue = (int(newBeachCountValueByGps))+int(predictedHourValue.val())/2*1.05;
+                newValue = (int(newBeachCountValueByGps)) + int(predictedHourValue.val());
+                newValue = newValue/2*1.5
+        data.child("Beaches/Country/" + beach.Country + "/" + beach.BeachID + "/Result").set(newValue)
+        data.child("Beaches/Country/" + beach.Country + "/" + beach.BeachID + "/Current People").set(
+            int(newBeachCountValueByGps))
 
-        str = "Beaches/"+str
-        data.child("Beaches/Country/Israel/Tzok/Result").set(newValue)
-        data.child("Beaches/Country/Israel/Tzok/Current People").set(int(newBeachCountValueByGps))
 
 def TimeSeriesAlogrithm():
     storage = firebase.storage()
-    beachList
+    print("test");
 
 
 config = {
-  "apiKey": "apiKey",
-  "authDomain": "https://zeach-ab079.firebaseio.com/",
-  "databaseURL": "https://zeach-ab079.firebaseio.com/",
-  "storageBucket": "zeach-ab079.appspot.com"
+    "apiKey": "apiKey",
+    "authDomain": "https://zeach-ab079.firebaseio.com/",
+    "databaseURL": "https://zeach-ab079.firebaseio.com/",
+    "storageBucket": "zeach-ab079.appspot.com"
 }
 
 firebase = pyrebase.initialize_app(config)
 data = firebase.database()
-mystream = data.child("BeachesListener/Country/Israel/Tzok/Current People").stream(stream_handler, stream_id="beach count")
-#storage = firebase.storage()
-#storage.child("Users/ofir.pdf").put("/Users/ofirmonis/Desktop/Flight confirmation.pdf",token=None)
+mystream = data.child("BeachesListener/Country").stream(stream_handler, stream_id="beach count")
+# storage = firebase.storage()
+# storage.child("Users/ofir.pdf").put("/Users/ofirmonis/Desktop/Flight confirmation.pdf",token=None)
 schech = BlockingScheduler();
 
-@schech.scheduled_job('cron', day_of_week='mon-sat', hour=21)
+
+@schech.scheduled_job('cron', day_of_week='mon-sat', hour=15)
 def scheduled_job():
-    #print('This job is run every weekday at 5pm.')
+    # print('This job is run every weekday at 5pm.')
     TimeSeriesAlogrithm()
+
+
 schech.start()
 ###
-
-
-
-
